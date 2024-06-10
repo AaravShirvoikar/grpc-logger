cad0d8e2922146ffe11c81ee218054217eeeab88
AaravShirvoikar <aaravshirvoikar685@gmail.com>
2024-06-11 02:08:04 +0530

    removed some unneeded helper functions

--- logger-client/cmd/api/handlers.go
+++ logger-client/cmd/api/handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 	"time"
 
@@ -9,6 +10,12 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
 
 type LogPayload struct {
 	Name string `json:"name"`
@@ -18,9 +25,9 @@ type LogPayload struct {
 func (app *Config) Log(w http.ResponseWriter, r *http.Request) {
 	var payload LogPayload
 
-	err := app.readJSON(w, r, &payload)
+	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
-		app.errorJSON(w, err)
+		errorJSON(w, err)
 		return
 	}
 
@@ -30,7 +37,7 @@ func (app *Config) Log(w http.ResponseWriter, r *http.Request) {
 func (app *Config) LogEvent(w http.ResponseWriter, r *http.Request, p LogPayload) {
 	conn, err := grpc.Dial("logger-service:50001", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
 	if err != nil {
-		app.errorJSON(w, err)
+		errorJSON(w, err)
 		return
 	}
 	defer conn.Close()
@@ -46,13 +53,26 @@ func (app *Config) LogEvent(w http.ResponseWriter, r *http.Request, p LogPayload
 		},
 	})
 	if err != nil {
-		app.errorJSON(w, err)
+		errorJSON(w, err)
 		return
 	}
 
 	var payload jsonResponse
 	payload.Error = false
 	payload.Message = "logged"
+	
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(payload)
+}
 
-	app.writeJSON(w, http.StatusAccepted, payload)
+func errorJSON(w http.ResponseWriter, err error) {
+	statusCode := http.StatusBadRequest
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(payload)
 }
